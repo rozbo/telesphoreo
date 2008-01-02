e55a76157743a6e8a14ba6c2e48405aba061410d
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-01-02 08:12:33 +0000

    Fixed a bunch of massively broken dependencies and got OpenSSH working.

--- trunk/data/openssh/make.sh
+++ trunk/data/openssh/make.sh
@@ -1,5 +1,9 @@
 pkg:extract
 cd *
-pkg:configure --disable-strip --sysconfdir=/etc
+pkg:configure --disable-strip --sysconfdir=/etc/ssh
 make
-pkg:install
+pkg:install INSTALL_SSH_RAND_HELPER=yes
+pkg: cp -a %/sshd-keygen-wrapper /usr/libexec
+pkg: mkdir -p /Library/LaunchDaemons
+pkg: cp -a %/com.openssh.sshd.plist /Library/LaunchDaemons
+pkg: cp -af %/ssh{d,}_config /etc/ssh
