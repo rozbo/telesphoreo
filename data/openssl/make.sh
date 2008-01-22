672dcf341b59135ad64ef0f08e8182ba0cfaadca
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-01-22 14:26:11 +0000

    Modernized OpenSSL's make.sh script and removed more of its manual pages.

--- data/openssl/make.sh
+++ data/openssl/make.sh
@@ -1,10 +1,10 @@
-tar -zxvf "${PKG_DATA}/openssl-0.9.8g.tar.gz"
-cd openssl-0.9.8g
+pkg:extract
+cd *
 pkg:patch
 ./Configure darwin-arm-gcc --prefix=/usr --openssldir=/usr/lib/ssl shared
 make AR='arm-apple-darwin-ar -r'
 make install INSTALL_PREFIX="${PKG_DEST}"
-rm -rf "${PKG_DEST}"/usr/lib/man
-mkdir -p "${PKG_DEST}"/etc/ssl
+pkg: rm -rf /usr/lib/man /usr/lib/ssl/man
+pkg: mkdir -p /etc/ssl
 mv "${PKG_DEST}"/usr/lib/ssl/{certs,openssl.cnf,private} "${PKG_DEST}"/etc/ssl
 ln -s /etc/ssl/{certs,openssl.cnf,private} "${PKG_DEST}"/usr/lib/ssl
