4fd4383dffa93bf2b476878584b7d7dce5998a5b
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-01-22 14:21:57 +0000

    Adding a fully working copy of Ruby.

--- data/ruby/make.sh
+++ data/ruby/make.sh
@@ -2,7 +2,11 @@ pkg:extract
 cd *
 pkg:patch
 autoconf
-pkg:configure ac_cv_func_setpgrp_void=yes rb_cv_stack_grow_dir=-1 --disable-rpath --enable-shared
+pkg:configure ac_cv_func_setpgrp_void=yes rb_cv_binary_elf=no rb_cv_stack_grow_dir=-1 rb_cv_bsd_signal=yes ac_cv_type_getgroups=gid_t ac_cv_c_stack_direction=1 ac_cv_func_memcmp_working=yes --enable-wide-getaddrinfo
 make
+cd ext/iconv
+touch iconv.c
+make LOCAL_LIBS=-liconv
+cd ../..
 pkg:install
-pkg: rm -rf /usr/share
+pkg: rm -f /usr/lib/libruby-static.a
