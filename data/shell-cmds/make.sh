a080ae9713ec834d4cd952008b3e8eb8345b8209
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-01-22 14:24:07 +0000

    Providing killall using the shell-cmds package.

--- data/shell-cmds/make.sh
+++ data/shell-cmds/make.sh
@@ -1,7 +1,7 @@
 tar -zxvf "${PKG_DATA}/shell_cmds-116.tar.gz"
 cd shell_cmds-116
 mkdir -p "${PKG_DEST}/usr/bin"
-for bin in mktemp renice script time which; do
+for bin in killall mktemp renice script time which; do
     arm-apple-darwin-gcc -o "${bin}/${bin}" "${bin}"/*.c -D'__FBSDID(x)='
     cp -a "${bin}/${bin}" "${PKG_DEST}/usr/bin"
 done
