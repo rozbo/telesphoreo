8dd1e7f59a3726943fd3a4fc4d788a7871dc33af
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-01-22 14:25:35 +0000

    Changed vim's build configuration to guarantee it doesn't link against X libraries.

--- trunk/data/vim/make.sh
+++ trunk/data/vim/make.sh
@@ -4,6 +4,6 @@ pkg:patch
 cd src
 autoconf
 cd ..
-STRIP=$(which arm-apple-darwin-strip) CFLAGS='-O0' ./configure --prefix=/usr --host=arm-apple-darwin vim_cv_toupper_broken=no --with-tlib=ncurses vim_cv_terminfo=yes vim_cv_tty_group=4 vim_cv_tty_mode=0620 vim_cv_getcwd_broken=no vim_cv_stat_ignores_slash=no ac_cv_sizeof_int=4 vim_cv_memmove_handles_overlap=yes
+STRIP=$(which arm-apple-darwin-strip) CFLAGS='-O0' ./configure --prefix=/usr --host=arm-apple-darwin --enable-gui=no --without-x vim_cv_toupper_broken=no --with-tlib=ncurses vim_cv_terminfo=yes vim_cv_tty_group=4 vim_cv_tty_mode=0620 vim_cv_getcwd_broken=no vim_cv_stat_ignores_slash=no ac_cv_sizeof_int=4 vim_cv_memmove_handles_overlap=yes
 make
 pkg:install
