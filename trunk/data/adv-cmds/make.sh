0671923b34bd7d9bdac43b5f66216ca89866d6b9
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-01-27 23:41:13 +0000

    Compile more of adv-cmds (finger, fingerd, last, md).

--- trunk/data/adv-cmds/make.sh
+++ trunk/data/adv-cmds/make.sh
@@ -1,9 +1,10 @@
 pkg:extract
 cd *
-for tproj in lsvfs ps; do
+for tproj in finger fingerd last lsvfs md ps; do
     arm-apple-darwin-gcc -o "${tproj}" "${tproj}.tproj"/*.c -D'__FBSDID(x)='
     arm-apple-darwin-strip "${tproj}"
 done
-pkg: mkdir -p /bin /usr/bin
+pkg: mkdir -p /bin /usr/bin /usr/libexec
 pkg: cp -a ps /bin
-pkg: cp -a lsvfs /usr/bin
+pkg: cp -a finger last lsvfs md /usr/bin
+pkg: cp -a fingerd /usr/libexec
