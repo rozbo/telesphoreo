e214a0da989c7de3164bd7cd9d74bf922d948b4b
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-03 13:21:58 +0000

    Finalizing some issues with the bootstrap package.

--- trunk/bootstrap.sh
+++ trunk/bootstrap.sh
@@ -3,11 +3,17 @@ set -e
 shopt -s extglob nullglob
 
 export PKG_BASE=$(realpath "$(dirname "$0")")
+export PKG_BOOT=${PKG_BASE}/Packager
 
-rm -rf "${PKG_BASE}/Packager"
-mkdir -p "${PKG_BASE}/Packager/var/lib/dpkg/info"
+rm -rf "${PKG_BOOT}"
+svn export "${PKG_BASE}/over" "${PKG_BOOT}"
 
-PKG_REQS=($(grep required "${PKG_BASE}"/data/!(*_)/_metadata/priority -l))
+mkdir -p "${PKG_BOOT}/var/lib/dpkg/info"
+
+PKG_REQS=(adv-cmds base cydia gawk grep inetutils nano network-cmds nvi saurik sed shell-cmds system-cmds unzip zip)
+
+cd "${PKG_BASE}/data"
+PKG_REQS=($(find -L "${PKG_REQS[@]}" | while read -r line; do realpath "${line}"; done | grep "/home/saurik/telesphoreo/data/[^/]*$" | sed -e 's/.*\///' | sort -u))
 
 for PKG_NAME in "${PKG_REQS[@]}"; do
     PKG_NAME=${PKG_NAME%/_metadata/priority}
@@ -16,19 +22,25 @@ for PKG_NAME in "${PKG_REQS[@]}"; do
     export PKG_DEST=${PKG_BASE}/dest/${PKG_NAME}
 
     echo "merging ${PKG_NAME}..."
-    cp -a "${PKG_DEST}"/* "${PKG_BASE}/Packager"
+    cp -a "${PKG_DEST}"/* "${PKG_BOOT}"
 
-    "${PKG_BASE}/control.sh" "${PKG_NAME}" available >>"${PKG_BASE}/Packager/var/lib/dpkg/available"
-    "${PKG_BASE}/control.sh" "${PKG_NAME}" status >>"${PKG_BASE}/Packager/var/lib/dpkg/status"
+    "${PKG_BASE}/control.sh" "${PKG_NAME}" available >>"${PKG_BOOT}/var/lib/dpkg/available"
+    "${PKG_BASE}/control.sh" "${PKG_NAME}" status >>"${PKG_BOOT}/var/lib/dpkg/status"
 
     (cd "${PKG_DEST}"; find | sed -e '
         s/^\.\///
         s/^/\//
-    ') >"${PKG_BASE}/Packager/var/lib/dpkg/info/${PKG_NAME}.list"
+    ') >"${PKG_BOOT}/var/lib/dpkg/info/${PKG_NAME}.list"
 done
 
-cp -a "${PKG_BASE}"/over/* "${PKG_BASE}/Packager"
-cd "${PKG_BASE}/Packager"
+cd "${PKG_BOOT}"
+
+rm -f ../Packager.xml
+find * -type l -print -o -name "terminfo" -prune | while read -r link; do
+    echo "<array><string>Exec</string><string>/bin/ln -fs \"$(readlink "${link}")\" \"/${link}\"</string></array>"
+    rm -f "${link}"
+done >>../Packager.xml
+
 rm -f ../Packager.zip
-zip -ry ../Packager.zip *
-rm -rf "${PKG_BASE}/Packager"
+zip -qry ../Packager.zip *
+rm -rf "${PKG_BOOT}"
