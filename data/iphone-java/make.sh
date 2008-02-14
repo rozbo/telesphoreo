a4276cb3a39b03a3f6ebbc4558db79d10a53c412
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-14 11:07:09 +0000

    Accidentally added a number of .svn files.

--- data/iphone-java/make.sh
+++ data/iphone-java/make.sh
@@ -2,3 +2,4 @@ pkg: mkdir /Applications
 javac -source 1.5 -d . -cp "$(PKG_DATA_ jocstrap)/jocstrap.jar:$(PKG_WORK_ javasqlite)/javasqlite-20071108/sqlite.jar" "${PKG_DATA}/HelloJava.app/HelloJava.java"
 pkg: cp -a %/Hello{Java,Script}.app /Applications
 cp -a *.class "${PKG_DEST}/Applications/HelloJava.app"
+pkg: find / -name '.svn' -prune -exec rm -rfv {} \;
