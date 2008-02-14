057fdb0f71933cfe5b780dd02a910326e5161c74
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-14 11:05:50 +0000

    JavaSQLite only needs Java 1.5.

--- data/javasqlite/make.sh
+++ data/javasqlite/make.sh
@@ -3,5 +3,5 @@ cd javasqlite-20071108
 pkg:patch
 autoconf
 pkg:configure --with-sqlite="${PKG_ROOT}/usr" --with-sqlite3="${PKG_ROOT}/usr"
-make
+JAVAC_FLAGS='-source 1.5 -target 1.5' make
 pkg:install
