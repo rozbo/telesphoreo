868136527ec73adf1f06c36535a9476aa479afd3
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-14 11:06:17 +0000

    Finally got 10.5 reboot working (with some serious hacks).

--- data/system-cmds/make.sh
+++ data/system-cmds/make.sh
@@ -13,8 +13,8 @@ arm-apple-darwin-gcc -o passwd passwd.tproj/!(od_passwd).c -I. -DTARGET_OS_EMBED
 arm-apple-darwin-strip passwd
 
 cp -va "${PKG_DATA}"/kextmanager* .
-# dmesg reboot shutdown
-for tproj in getconf getty hostinfo iostat login nvram sync sysctl vipw zprint; do
+# dmesg shutdown
+for tproj in getconf getty hostinfo iostat login nvram reboot sync sysctl vipw zprint; do
     echo "${tproj}"
     arm-apple-darwin-gcc -o "${tproj}" "${tproj}.tproj"/*.c -I. -D'__FBSDID(x)=' -DTARGET_OS_EMBEDDED -framework CoreFoundation -framework IOKit kextmanagerUser.c
     arm-apple-darwin-strip "${tproj}"
@@ -22,8 +22,9 @@ done
 
 chmod u+s passwd login
 
-pkg: mkdir -p /bin /usr/bin /usr/sbin
+pkg: mkdir -p /bin /sbin /usr/bin /usr/sbin
 
 pkg: cp -a sync /bin
+pkg: cp -a reboot /sbin
 pkg: cp -a passwd zprint getty getconf hostinfo login /usr/bin
 pkg: cp -a sysctl nvram vipw iostat /usr/sbin
