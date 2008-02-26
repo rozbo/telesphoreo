776789b5630ca16ffec01032a019ab6f4c46b6f6
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-26 12:27:31 +0000

    Upgrading to launch copy of Cydia.

--- trunk/data/cydia/make.sh
+++ trunk/data/cydia/make.sh
@@ -7,8 +7,8 @@ pkg: mkdir -p /var/lib/cydia
 
 pkg: mkdir /Applications
 pkg: cp -a Cydia.app /Applications
-pkg: cp -a Cydia /Applications/Cydia.app
-pkg: chmod +s /Applications/Cydia.app/Cydia
+pkg: cp -a Cydia /Applications/Cydia.app/Cydia_
+pkg: chmod +s /Applications/Cydia.app/Cydia_
 
 pkg: mkdir -p /System/Library/PreferenceBundles
 pkg: cp -a CydiaSettings.bundle /System/Library/PreferenceBundles
