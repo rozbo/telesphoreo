b55a20d47275eca20e802d1a6e32a09b96040346
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-27 23:51:48 +0000

    Added dmesg to system-cmds.

--- trunk/data/system-cmds/make.sh
+++ trunk/data/system-cmds/make.sh
@@ -12,11 +12,20 @@ cd ..
 arm-apple-darwin-gcc -o passwd passwd.tproj/!(od_passwd).c -I. -DTARGET_OS_EMBEDDED
 arm-apple-darwin-strip passwd
 
+arm-apple-darwin-gcc -o dmesg dmesg.tproj/*.c -I.
+arm-apple-darwin-strip dmesg
+
 cp -va "${PKG_DATA}"/kextmanager* .
-# dmesg shutdown
+# XXX: shutdown
 for tproj in getconf getty hostinfo iostat login nvram reboot sync sysctl vipw zprint; do
+    cflags=
+
+    case ${tproj} in (shutdown)
+        cflags="${cflags} -lbsm"
+    ;; esac
+
     echo "${tproj}"
-    arm-apple-darwin-gcc -o "${tproj}" "${tproj}.tproj"/*.c -I. -D'__FBSDID(x)=' -DTARGET_OS_EMBEDDED -framework CoreFoundation -framework IOKit kextmanagerUser.c
+    arm-apple-darwin-gcc -o "${tproj}" "${tproj}.tproj"/*.c -I. -D'__FBSDID(x)=' -DTARGET_OS_EMBEDDED -framework CoreFoundation -framework IOKit kextmanagerUser.c ${cflags}
     arm-apple-darwin-strip "${tproj}"
 done
 
@@ -25,6 +34,6 @@ chmod u+s passwd login
 pkg: mkdir -p /bin /sbin /usr/bin /usr/sbin
 
 pkg: cp -a sync /bin
-pkg: cp -a reboot /sbin
+pkg: cp -a reboot dmesg /sbin
 pkg: cp -a passwd zprint getty getconf hostinfo login /usr/bin
 pkg: cp -a sysctl nvram vipw iostat /usr/sbin
