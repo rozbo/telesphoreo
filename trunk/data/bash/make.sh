0562ee894fbda9874e60578e1afe82dc08f68f90
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-28 14:38:37 +0000

    Installer doesn't actually take root priviledges, so I just broke bash for it. I hope they are happy.

--- trunk/data/bash/make.sh
+++ trunk/data/bash/make.sh
@@ -1,5 +1,6 @@
-tar -zxvf "${PKG_DATA}/bash-3.2.tar.gz"
-cd bash-3.2
+pkg:extract
+cd *
+pkg:patch
 pkg:configure bash_cv_dev_fd=absent bash_cv_sys_named_pipes=present bash_cv_job_control_missing=missing
 make
 pkg:install
