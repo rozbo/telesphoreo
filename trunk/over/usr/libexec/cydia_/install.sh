8d77a3256c104ad4ad301f7fd6ae5c01c4c31bab
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-02-29 20:53:29 +0000

    Toughened up the bootstrap to deal with a few more border cases found in the wild.

--- trunk/over/usr/libexec/cydia_/install.sh
+++ trunk/over/usr/libexec/cydia_/install.sh
@@ -11,6 +11,34 @@ function df_() {
     echo "${free}"
 }
 
+function mv_() {
+    src=$1
+    dst=/var/$(basename "${src}")
+
+    if [[ -e ${dst} ]]; then
+        dst=$(mktemp -d /var/cydia.XXXXXX)
+    else
+        mkdir -p "${dst}"
+    fi
+
+    if [[ -e ${src} ]]; then
+        chmod --reference="${src}" "${dst}"
+        chown --reference="${src}" "${dst}"
+
+        cp -aT "${src}" "${dst}" || {
+            rm -rf "${dst}"
+            exit 1
+        }
+
+        rm -rf "${src}"
+    else
+        chmod 775 "${dst}"
+        chown root.admin "${dst}"
+    fi
+
+    ln -s "${dst}" "${src}"
+}
+
 for dir in \
     /Applications \
     /Library/Ringtones \
@@ -23,16 +51,11 @@ do if [[ -d ${dir} && ! -h ${dir} ]]; then
     free=$(df_ /var)
 
     if [[ $((used + 524288)) -lt ${free} ]]; then
-        base=$(basename "${dir}")
-
-        new=/var/${base}
-        if [[ -e ${new} ]]; then
-            new=$(godmode mktemp -d cydia.XXXXXX)
-        fi
-
-        godmode mv -T "${dir}" "${new}"
-        godmode ln -s "${new}" "${dir}"
+        mv_ "${dir}"
     fi
+elif [[ -h ${dir} && ! -e ${dir} ]]; then
+    rm -f "${dir}"
+    mv_ "${dir}"
 fi; done
 
 [[ $(df_ /) -gt 35651584 && $(df_ /var) -gt 35651584 ]]
