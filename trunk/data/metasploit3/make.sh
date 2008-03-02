23bd93d132e172a60956d9897280bc7adab1c39f
extremis <extremis@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-03-02 08:28:27 +0000

    Updated version to support the recent minor version change to 3.2.

--- trunk/data/metasploit3/make.sh
+++ trunk/data/metasploit3/make.sh
@@ -5,10 +5,10 @@ pkg: chmod 755 /usr/share/msf3/msfelfscan
 ln -s /usr/share/msf3/msfcli "${PKG_DEST}"/usr/bin/msfcli
 ln -s /usr/share/msf3/msfconsole "${PKG_DEST}"/usr/bin/msfconsole
 ln -s /usr/share/msf3/msfd "${PKG_DEST}"/usr/bin/msfd
+ln -s /usr/share/msf3/msfelfscan "${PKG_DEST}"/usr/bin/msfelfscan
 ln -s /usr/share/msf3/msfencode "${PKG_DEST}"/usr/bin/msfencode
 ln -s /usr/share/msf3/msfgui "${PKG_DEST}"/usr/bin/msfgui
 ln -s /usr/share/msf3/msfopcode "${PKG_DEST}"/usr/bin/msfopcode
 ln -s /usr/share/msf3/msfpayload "${PKG_DEST}"/usr/bin/msfpayload
 ln -s /usr/share/msf3/msfpescan "${PKG_DEST}"/usr/bin/msfpescan
 ln -s /usr/share/msf3/msfweb "${PKG_DEST}"/usr/bin/msfweb
-ln -s /usr/share/msf3/msfelfscan "${PKG_DEST}"/usr/bin/msfelfscan
