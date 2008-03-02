14c99e06d0e76682f09ac328a7926fd85180820d
extremis <extremis@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-03-02 05:07:26 +0000

    Initial Metasploit Framework 3.1 release. This version runs with reasonable stability and speed. You might notice a slight delay loading msf*. The only known issue includes a segfault when loading msfweb.

---
+++ data/metasploit3/make.sh
@@ -0,0 +1,12 @@
+tar -zxvf "${PKG_DATA}/framework-3.1.tar.gz"
+pkg: mkdir -p /var/local /usr/bin
+pkg: cp -a framework-3.1 /var/local/msf3
+pkg: ln -s /var/local/msf3/msfcli /usr/bin/msfcli
+pkg: ln -s /var/local/msf3/msfconsole /usr/bin/msfconsole
+pkg: ln -s /var/local/msf3/msfd /usr/bin/msfd
+pkg: ln -s /var/local/msf3/msfencode /usr/bin/msfencode
+pkg: ln -s /var/local/msf3/msfgui /usr/bin/msfgui
+pkg: ln -s /var/local/msf3/msfopcode /usr/bin/msfopcode
+pkg: ln -s /var/local/msf3/msfpayload /usr/bin/msfpayload
+pkg: ln -s /var/local/msf3/msfpescan /usr/bin/msfpescan
+pkg: ln -s /var/local/msf3/msfweb /usr/bin/msfweb
