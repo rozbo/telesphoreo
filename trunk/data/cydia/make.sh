3249212f4c616eeef2b996e5c6d92123dc042a79
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-03-03 00:15:39 +0000

    Merged to Cydia 2340.

--- trunk/data/cydia/make.sh
+++ trunk/data/cydia/make.sh
@@ -3,7 +3,12 @@ cd *
 pkg:patch
 make
 
-pkg: mkdir -p /var/lib/cydia
+pkg: mkdir -p /usr/libexec /var/lib/cydia
+pkg: cp -a Library /usr/libexec/cydia
+pkg: cp -a %/alert /usr/libexec/cydia
+
+pkg: mkdir -p /System/Library/LaunchDaemons
+pkg: mv /usr/libexec/cydia/com.saurik.Cydia.Firmware /System/Library/LaunchDaemons
 
 pkg: mkdir /Applications
 pkg: cp -a Cydia.app /Applications
