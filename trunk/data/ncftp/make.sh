9bb59be356e9d9f2e93a777f62793435288e1244
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-03-08 11:54:45 +0000

    Added the mDNSResponder fix to a few packages.

--- trunk/data/ncftp/make.sh
+++ trunk/data/ncftp/make.sh
@@ -1,5 +1,6 @@
-tar -zxvf "${PKG_DATA}/ncftp-3.2.1-src.tar.gz"
-cd ncftp-3.2.1
+pkg:extract
+cd *
+pkg:patch
 CC=$(which arm-apple-darwin-gcc) ./configure --prefix=/usr
 make
 mkdir -p "${PKG_DEST}/usr/share"
