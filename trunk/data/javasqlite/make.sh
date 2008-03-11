33459964bad04d9832dacd2df0bef28293945c85
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-03-11 03:37:32 +0000

    Upgraded to the latest JavaSQLite (20080130).

--- trunk/data/javasqlite/make.sh
+++ trunk/data/javasqlite/make.sh
@@ -1,5 +1,5 @@
-tar -zxvf "${PKG_DATA}/javasqlite-20071108.tar.gz"
-cd javasqlite-20071108
+pkg:extract
+cd *
 pkg:patch
 autoconf
 pkg:configure --with-sqlite="${PKG_ROOT}/usr" --with-sqlite3="${PKG_ROOT}/usr"
