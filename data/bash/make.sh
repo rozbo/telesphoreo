f1b13bf2be563858b5b23dd17b9279642201f14a
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-03-17 07:37:58 +0000

    Noticed some autoconf variables were either misconfigured or disconfigured. Apparently Brian loved autoconf, and so I didn't even finish configuring before I gave up for the night.

--- data/bash/make.sh
+++ data/bash/make.sh
@@ -1,7 +1,8 @@
 pkg:extract
 cd *
 pkg:patch
-pkg:configure bash_cv_dev_fd=absent bash_cv_sys_named_pipes=present bash_cv_job_control_missing=missing
+pkg:configure bash_cv_dev_fd=absent bash_cv_sys_named_pipes=missing bash_cv_job_control_missing=present bash_cv_func_sigsetjmp=present bash_cv_func_ctype_nonascii=no bash_cv_must_reinstall_sighandlers=no bash_cv_func_strcoll_broken=yes ac_cv_c_stack_direction=-1 ac_cv_func_mmap_fixed_mapped=yes gt_cv_int_divbyzero_sigfpe=no ac_cv_func_setvbuf_reversed=no ac_cv_func_strcoll_works=yes ac_cv_func_working_mktime=yes ac_cv_type_getgroups=gid_t bash_cv_dup2_broken=no
+#pgrp_pipe...
 make
 pkg:install
 pkg:bin bash
