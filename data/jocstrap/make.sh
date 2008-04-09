30963e839efa37eba3ce27014454a9c47aee40ad
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-09 13:16:30 +0000

    Upgraded a few projects to their latest builds.

--- data/jocstrap/make.sh
+++ data/jocstrap/make.sh
@@ -1,3 +1,7 @@
+pkg:extract
+cd jocstrap-*
+make
 pkg: mkdir -p /usr/lib /usr/share/java
-pkg: cp %/libjocstrap.jnilib /usr/lib
-pkg: cp %/jocstrap.jar /usr/share/java
+pkg: cp -a libobjc-sig.dylib /usr/lib
+pkg: cp -a libjocstrap.jnilib /usr/lib
+pkg: cp -a jocstrap.jar /usr/share/java
