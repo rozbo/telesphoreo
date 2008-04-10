79b0154224a5fe39f7e7937cc820aef6af8dcce0
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-10 07:41:26 +0000

    Fixed cross-package dependencies on iphone-java.

--- data/iphone-java/make.sh
+++ data/iphone-java/make.sh
@@ -1,5 +1,5 @@
 pkg: mkdir /Applications
-javac -source 1.5 -d . -cp "$(PKG_DATA_ jocstrap)/jocstrap.jar:$(PKG_WORK_ javasqlite)/javasqlite-20080130/sqlite.jar" "${PKG_DATA}/HelloJava.app/HelloJava.java"
+javac -source 1.5 -d . -cp "$(PKG_DEST_ jocstrap)/usr/share/java/jocstrap.jar:$(PKG_DEST_ javasqlite)/usr/share/java/sqlite.jar" "${PKG_DATA}/HelloJava.app/HelloJava.java"
 pkg: cp -a %/Hello{Java,Script}.app /Applications
 cp -a *.class "${PKG_DEST}/Applications/HelloJava.app"
 pkg: find / -name '.svn' -prune -exec rm -rfv {} \;
