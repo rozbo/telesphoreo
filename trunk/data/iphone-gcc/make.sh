4015e2a49c65946c2b4f4a4fd4c10009375df83f
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-10 07:40:35 +0000

    Added gcc and csu for native development.

---
+++ trunk/data/iphone-gcc/make.sh
@@ -0,0 +1,11 @@
+shopt -s extglob
+pkg:extract
+mkdir llvm-gcc-bld
+cd llvm-gcc-bld
+PKG_CONF=../llvm-gcc-4.2/configure pkg:configure \
+    --enable-languages="c,c++,objc,obj-c++" \
+    --enable-wchar_t="no" \
+    --with-gxx-include-dir="/usr/include/c++/4.0.0"
+make -j3
+pkg:install
+pkg: rm -f /usr/lib/libgcc_s.1.dylib
