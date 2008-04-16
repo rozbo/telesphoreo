797b8fcf8498295a1c5b6d430221302e14e846dd
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-16 07:15:29 +0000

    Retooling for the new bootstrap packages.

--- trunk/build.sh
+++ trunk/build.sh
@@ -2,7 +2,7 @@
 set -e
 shopt -s extglob
 #for package in coreutils berkeleydb bzip2 apt adv-cmds libutil gettext gawk tar network-cmds shell-cmds odcctools readline modmyifone darwintools sed pcre gnupg grep inetutils saurik gzip dpkg iphonesurge unzip libarmfp nano base bash system-cmds libresolv zip ncurses less; do
-for package in data/!(*_|cydia|iphone-python|mobileterminal|jocstrap|iphone-java|uicaboodle|pyobjc|python|setuptools); do
+for package in data/!(*_|cydia|iphone-python|mobileterminal|jocstrap|iphone-java|uicaboodle|pyobjc|python|setuptools|uicaboodle.py); do
 #for package in data/!(*_); do
     PKG_NAME=$(basename "${package}")
     echo "========== ${PKG_NAME} =========="
