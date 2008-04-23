7a4cbd2b0d1afeb7369366f70009abf3da88584e
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-23 21:22:19 +0000

    Moved nanorc to the correct folder and added a good default.

--- trunk/data/nano/make.sh
+++ trunk/data/nano/make.sh
@@ -1,6 +1,7 @@
 pkg:setup
-pkg:configure --enable-all
+pkg:configure --enable-all --sysconfdir=/etc
 make
 pkg:install
 pkg: mkdir -p /etc/profile.d
 pkg: cp -a %/nano.sh /etc/profile.d
+pkg: cp -a %/nanorc /etc
