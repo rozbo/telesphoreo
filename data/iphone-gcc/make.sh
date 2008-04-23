69e1628493f5fedb4cec47c1d7ed25f0c12bf2af
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-23 21:22:00 +0000

    Fully compatibilized iphone-gcc with the libgcc package.

--- data/iphone-gcc/make.sh
+++ data/iphone-gcc/make.sh
@@ -8,5 +8,6 @@ PKG_CONF=../llvm-gcc-4.2/configure pkg:configure \
     --with-gxx-include-dir="/usr/include/c++/4.0.0"
 make -j3
 pkg:install
-pkg: rm -f /usr/lib/libgcc_s.1.dylib
+pkg: rm -f /usr/lib/libgcc_s.{1,10.4,10.5}.dylib
+pkg: rm -f /usr/lib/libgcc_s_{ppc64,x86_64}.1.dylib
 pkg: rm -f /usr/lib/libiberty.a
