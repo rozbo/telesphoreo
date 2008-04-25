c0a3a48deeb01119a3269a5d90d8ca46111839bc
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-25 18:57:26 +0000

    Added a key to ModMyiFone's repository.

--- data/modmyifone/make.sh
+++ data/modmyifone/make.sh
@@ -1,2 +1,4 @@
 pkg: mkdir -p /etc/apt/sources.list.d
 pkg: cp -a %/modmyifone.list /etc/apt/sources.list.d
+pkg: mkdir -p /usr/share/keyrings
+pkg: cp -a %/186902.txt /usr/share/keyrings/modmyifone-keyring.gpg
