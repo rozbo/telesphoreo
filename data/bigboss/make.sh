65f2e4e871359344f1d064a2f07c6e34e3f0e89c
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-04-26 17:59:49 +0000

    Added key for BigBoss's repository.

--- data/bigboss/make.sh
+++ data/bigboss/make.sh
@@ -1,2 +1,4 @@
 pkg: mkdir -p /etc/apt/sources.list.d
 pkg: cp -a %/bigboss.list /etc/apt/sources.list.d
+pkg: mkdir -p /usr/share/keyrings
+pkg: cp -a %/bigboss.asc /usr/share/keyrings/bigboss-keyring.gpg
