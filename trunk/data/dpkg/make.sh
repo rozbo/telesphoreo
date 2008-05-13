be44587e806db20243c18f7898b661f34283cd00
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-05-13 06:37:06 +0000

    Added dselect support, and fixed dependencies.

--- trunk/data/dpkg/make.sh
+++ trunk/data/dpkg/make.sh
@@ -1,6 +1,6 @@
 pkg:setup
 autoconf
 cp -a getopt/obstack.[ch] lib
-pkg:configure --with-admindir=/var/lib/dpkg --without-start-stop-daemon --without-dselect --disable-nls --sysconfdir=/etc --disable-linker-optimisations
+pkg:configure --with-admindir=/var/lib/dpkg --without-start-stop-daemon --disable-nls --sysconfdir=/etc --disable-linker-optimisations dpkg_cv_va_copy=yes
 make
 pkg:install
