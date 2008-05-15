bef56db1f67f075acb9ea60dedcde5da1e758e37
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-05-15 22:42:59 +0000

    Added ncursesw support.

--- trunk/data/ncurses/make.sh
+++ trunk/data/ncurses/make.sh
@@ -1,4 +1,12 @@
 pkg:setup
-pkg:configure --with-shared --without-normal --without-debug
+cd ..
+dir=$(echo *)
+mkdir bld-ncurses{,w}
+cd bld-ncurses
+PKG_CONF=../${dir}/configure pkg:configure --with-shared --without-normal --without-debug --enable-sigwinch
+make
+pkg:install
+cd ../bld-ncursesw
+PKG_CONF=../${dir}/configure pkg:configure --with-shared --without-normal --without-debug --enable-sigwinch --disable-overwrite --enable-widec
 make
 pkg:install
