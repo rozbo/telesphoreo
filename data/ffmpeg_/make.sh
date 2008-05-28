834b96e11709cc1ecd30b04b0219c5a43bd9c339
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-05-28 09:39:25 +0000

    Merged to the latest ffmpeg and added some stuff.

--- data/ffmpeg_/make.sh
+++ data/ffmpeg_/make.sh
@@ -1,8 +1,10 @@
 pkg:setup
 ./configure \
-    --cross-compile --cross-prefix=${PKG_TARG}- --target-os=darwin \
+    --prefix=/usr --enable-gpl --enable-postproc --enable-swscale \
+    --disable-vhook --enable-avfilter --enable-avfilter-lavf \
+    --enable-cross-compile --cross-prefix=${PKG_TARG}- --target-os=darwin \
     --arch=arm --disable-iwmmxt --enable-armv5te --enable-armv6 \
-    --enable-static --disable-shared --disable-debug --prefix=/usr --enable-gpl \
+    --enable-static --disable-shared --disable-debug --disable-stripping \
     --enable-libfaac \
     --enable-libfaad \
     --enable-libmp3lame \
