2c15e282f84c5e6f45e05c2a10c47c92a004ab61
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-15 06:43:06 +0000

    JamVM should provide jvm and should setup the BOOTCLASSPATH in such a way that jikes works out of the box.

--- trunk/data/jamvm/make.sh
+++ trunk/data/jamvm/make.sh
@@ -5,3 +5,5 @@ make with_classpath_install_dir="$(PKG_DEST_ classpath)/usr"
 pkg:install
 pkg: rm -rf /usr/include
 pkg: ln -s jamvm /usr/bin/java
+pkg: mkdir -p /etc/profile.d
+pkg: cp -a %/jamvm.sh /etc/profile.d
