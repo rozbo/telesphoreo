19a3831fcbcd43fb5a07d556d9bc8d9d600396b4
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-17 22:20:47 +0000

    Deterministicized libresolv by saving temporary object files.

--- data/libresolv/make.sh
+++ data/libresolv/make.sh
@@ -3,6 +3,6 @@ cp -a "${PKG_DATA}"/*.[ch] .
 declare -a flags
 # XXX: don't use this on darwin8
 flags[${#flags[@]}]=-DNEED_PSELECT
-"${PKG_TARG}-gcc" -fno-common !(dns_async).c -o libresolv.dylib -dynamiclib -I. -linfo -install_name /usr/lib/libresolv.dylib "${flags[@]}"
+"${PKG_TARG}-gcc" -fno-common !(dns_async).c -o libresolv.dylib -dynamiclib -I. -linfo -install_name /usr/lib/libresolv.dylib "${flags[@]}" -save-temps
 pkg: mkdir -p /usr/lib
 pkg: cp -a libresolv.dylib /usr/lib
