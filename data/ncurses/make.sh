cae74cac9b8b49f1d189e659607efe8ba4e92a4a
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-18 07:23:15 +0000

    Minified terminfo database for smaller bootstrap.

--- data/ncurses/make.sh
+++ data/ncurses/make.sh
@@ -1,12 +1,24 @@
+shopt -s extglob
 pkg:setup
 cd ..
 dir=$(echo *)
 mkdir bld-ncurses{,w}
 cd bld-ncurses
-PKG_CONF=../${dir}/configure pkg:configure --with-shared --without-normal --without-debug --enable-sigwinch
+flags='--with-shared --without-normal --without-debug --enable-sigwinch'
+PKG_CONF=../${dir}/configure pkg:configure ${flags}
 make
 pkg:install
 cd ../bld-ncursesw
-PKG_CONF=../${dir}/configure pkg:configure --with-shared --without-normal --without-debug --enable-sigwinch --disable-overwrite --enable-widec
+PKG_CONF=../${dir}/configure pkg:configure ${flags} --disable-overwrite --enable-widec
 make
 pkg:install
+
+for ti in "${PKG_DEST}"/usr/share/terminfo/*/*; do
+    if [[ ${ti} == */@(?(pc)ansi|cons25|cygwin|dumb|linux|mach|rxvt|screen|sun|vt@(52|100|102|220)|swvt25?(m)|[Ex]term)?(-*) ]]; then
+        echo "keeping terminfo: ${ti}"
+    else
+        rm -f "${ti}"
+    fi
+done
+
+rmdir --ignore-fail-on-non-empty "${PKG_DEST}"/usr/share/terminfo/*
