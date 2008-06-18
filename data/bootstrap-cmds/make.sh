d40f707340ec39008ad793f2d43f43fc364633be
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-18 07:22:07 +0000

    Went through and did my best to remove timestamping.

--- data/bootstrap-cmds/make.sh
+++ data/bootstrap-cmds/make.sh
@@ -1,13 +1,9 @@
 pkg:setup
 cd migcom.tproj
-cat >ident.c <<EOF
-char *MigGenerationDate = "`date`";
-char *MigMoreData = "`whoami`@`hostname`";
-EOF
 cp -a i386 arm
 yacc -d parser.y -o parser.c
 lex lexxer.l
-${PKG_TARG}-gcc -o migcom *.c
+${PKG_TARG}-gcc -o migcom *.c -save-temps
 pkg: mkdir -p /usr/bin /usr/libexec
 pkg: cp -a migcom /usr/libexec
 pkg: cp -a mig.sh /usr/bin/mig
--- data/git/make.sh
+++ data/git/make.sh
@@ -2,3 +2,4 @@ pkg:setup
 pkg:configure ac_cv_c_c99_format=yes ac_cv_fread_reads_directories=no ac_cv_snprintf_returns_bogus=yes --without-tcltk
 make
 pkg:install
+pkg: sed -i -e 's/^=head2 .*:/=head2/' /usr/lib/perl/5.8.8/perllocal.pod
