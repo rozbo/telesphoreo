f18326291a77d50f1160a9b2c55eb8ded81e09e2
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-18 07:22:52 +0000

    Fixed libiconv detection in glib.

--- trunk/data/glib/make.sh
+++ trunk/data/glib/make.sh
@@ -1,5 +1,5 @@
 pkg:setup
 autoconf
-pkg:configure glib_cv_stack_grows=no glib_cv_uscore=yes ac_cv_func_posix_getpwuid_r=yes
+pkg:configure glib_cv_stack_grows=no glib_cv_uscore=yes ac_cv_func_posix_getpwuid_r=yes --with-libiconv=native
 make
 pkg:install
