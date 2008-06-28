c60c0d6e1611863b4d07995db69070622e933d7d
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-28 18:44:13 +0000

    Minor biuld determinism mistakes.

--- data/shell-cmds/make.sh
+++ data/shell-cmds/make.sh
@@ -1,7 +1,7 @@
 pkg:setup
 mkdir -p "${PKG_DEST}/usr/bin"
 for bin in killall mktemp renice script time which; do
-    ${PKG_TARG}-gcc -o "${bin}/${bin}" "${bin}"/*.c -D'__FBSDID(x)='
+    ${PKG_TARG}-gcc -o "${bin}/${bin}" "${bin}"/*.c -D'__FBSDID(x)=' -save-temps
     cp -a "${bin}/${bin}" "${PKG_DEST}/usr/bin"
 done
 pkg:bin mktemp
