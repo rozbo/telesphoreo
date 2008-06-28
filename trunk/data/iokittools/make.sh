9ea9a54db395c37110c38b210321a86c28096fb7
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-28 18:42:16 +0000

    Minor biuld determinism mistakes.

--- trunk/data/iokittools/make.sh
+++ trunk/data/iokittools/make.sh
@@ -3,6 +3,6 @@ pkg:setup
 pkg: mkdir -p /usr/sbin
 for tproj in *.tproj; do
     tproj=$(basename "${tproj}" .tproj)
-    ${PKG_TARG}-gcc -lcurses -o "${tproj}" "${tproj}.tproj"/*.c -framework CoreFoundation -framework IOKit
+    ${PKG_TARG}-gcc -lcurses -o "${tproj}" "${tproj}.tproj"/*.c -framework CoreFoundation -framework IOKit -save-temps
     pkg: cp -a "${tproj}" /usr/sbin
 done
