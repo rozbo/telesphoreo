1b4a79be7240df16733d91511dfe07e75918a9b0
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-06-28 18:46:41 +0000

    Missing architectures should be handled.

--- trunk/architect.sh
+++ trunk/architect.sh
@@ -1,1 +1,6 @@
+if [[ ! -e ${PKG_BASE}/arch/${PKG_ARCH}/target ]]; then
+    echo "unknown architecture: ${PKG_NAME}" 1>&2
+    exit 1
+fi
+
 export PKG_TARG=$(cat "${PKG_BASE}/arch/${PKG_ARCH}/target")
