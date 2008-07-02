9cc1f73740ca98daf4221fe0d837015d68d36230
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-07-02 08:13:38 +0000

    Finally merging latest builds of JocStrap and UICaboodle, bringing the latter build environment in-house.

--- data/jocstrap/make.sh
+++ data/jocstrap/make.sh
@@ -1,7 +1,9 @@
 pkg:extract
 cd jocstrap-*
-make
-pkg: mkdir -p /usr/lib /usr/share/java
+# XXX: omg this is stupid
+PKG_ROOT2=/apl/sys/ad8 make
+pkg: mkdir -p /usr/include/joc /usr/lib /usr/share/java
+cp -a source/joc/*.h "${PKG_DEST}"/usr/include/joc
 pkg: cp -a libobjc-sig.dylib /usr/lib
 pkg: cp -a libjocstrap.jnilib /usr/lib
 pkg: cp -a jocstrap.jar /usr/share/java
