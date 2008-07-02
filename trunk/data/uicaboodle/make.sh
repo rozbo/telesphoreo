a9e7be07b2a029c9f978a0f94e9243d4b992852a
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-07-02 08:13:38 +0000

    Finally merging latest builds of JocStrap and UICaboodle, bringing the latter build environment in-house.

--- trunk/data/uicaboodle/make.sh
+++ trunk/data/uicaboodle/make.sh
@@ -1,3 +1,7 @@
+pkg:extract
+cd uicaboodle.java-*
+mv ../minimal source/com/saurik/uicaboodle
+./ant.sh
 pkg: mkdir -p /usr/lib /usr/share/java
-pkg: cp %/libuicaboodle.jnilib /usr/lib
-pkg: cp %/uicaboodle.jar /usr/share/java
+pkg: cp -a archives/libuicaboodle.jnilib /usr/lib
+pkg: cp -a archives/uicaboodle.jar /usr/share/java
