6a00e3995dce3d5945700e4eaa545e799ff4cb36
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-07-20 04:40:32 +0000

    Massive changes to support 2.0.

--- data/mobileterminal/make.sh
+++ data/mobileterminal/make.sh
@@ -1,4 +1,6 @@
 pkg:setup
+echo "#define SVN_VERSION @\"${PKG_VERS}\"" >svnversion.h
+make clean
 make -f Makefile.build CC="${PKG_TARG}-gcc"
 pkg: mkdir -p /Applications/Terminal.app
 pkg: cp -a Info.plist Resources/* Terminal /Applications/Terminal.app
