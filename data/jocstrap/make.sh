f37ee1789fb490fbde1d15a2563cfb4ddc6412cd
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-07-20 14:55:47 +0000

    Go go JocStrap 2.0.

--- data/jocstrap/make.sh
+++ data/jocstrap/make.sh
@@ -1,7 +1,7 @@
-pkg:extract
-cd jocstrap-*
+pkg:setup
+cd jocstrap
 # XXX: omg this is stupid
-PKG_ROOT2=/apl/sys/ad8 make
+PKG_ROOT2=${PKG_BASE}/arch/${PKG_ARCH}/dumpable make
 pkg: mkdir -p /usr/include/joc /usr/lib /usr/share/java
 cp -a source/joc/*.h "${PKG_DEST}"/usr/include/joc
 pkg: cp -a libobjc-sig.dylib /usr/lib
