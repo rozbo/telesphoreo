37303129429bd88dcdcead71dae0ea34fcb1160f
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-07-25 03:34:54 +0000

    Fixed up extremis' metasploit file.

--- trunk/data/metasploit3/make.sh
+++ trunk/data/metasploit3/make.sh
@@ -2,7 +2,7 @@ pkg:extract
 pkg: mkdir -p /usr/share /usr/bin
 pkg: cp -a * /usr/share/msf3
 pkg: find / -name '.svn' -prune -exec rm -rfv {} \;
-${PKG_TARG}-gcc -o /usr/share/msf3/data/templates/template_armle_darwin.bin /usr/share/msf3/data/templates/template.c
+pkg: "${PKG_TARG}-gcc" -o /usr/share/msf3/data/templates/template_armle_darwin.bin /usr/share/msf3/data/templates/template.c
 ln -s /usr/share/msf3/msfcli "${PKG_DEST}"/usr/bin/msfcli
 ln -s /usr/share/msf3/msfconsole "${PKG_DEST}"/usr/bin/msfconsole
 ln -s /usr/share/msf3/msfd "${PKG_DEST}"/usr/bin/msfd
