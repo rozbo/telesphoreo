340f09b4b7807d92ce531f914c72e31077f8fcab
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-08-11 11:03:20 +0000

    Upgraded to the latest Cydia.

--- data/cydia/make.sh
+++ data/cydia/make.sh
@@ -5,7 +5,6 @@ make
 pkg: mkdir -p /usr/libexec /var/lib/cydia
 pkg: cp -a Library /usr/libexec/cydia
 pkg: cp -a exec /usr/libexec/cydia
-pkg: cp -a %/alert /usr/libexec/cydia
 
 pkg: mkdir -p /System/Library
 pkg: cp -a LaunchDaemons /System/Library
@@ -14,8 +13,6 @@ pkg: mkdir /Applications
 pkg: cp -a Cydia.app /Applications
 pkg: cp -a Cydia /Applications/Cydia.app/Cydia_
 pkg: chmod +s /Applications/Cydia.app/Cydia_
-
-pkg: mv /Applications/Cydia.app/Sources/iphonehe.{,com.png}
 
 pkg: mkdir -p /System/Library/PreferenceBundles
 pkg: cp -a CydiaSettings.bundle /System/Library/PreferenceBundles
