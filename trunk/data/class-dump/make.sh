d6343b8574145e3d5e5931fc70f83ed7119f4dec
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-08-19 01:40:16 +0000

    Upgraded to class-dump-x.

--- trunk/data/class-dump/make.sh
+++ trunk/data/class-dump/make.sh
@@ -1,5 +1,5 @@
 pkg:setup
 cd src
-"${PKG_TARG}-gcc" -o class-dump -ObjC *.m -Wall -include Foundation/Foundation.h -framework CoreFoundation -framework Foundation -lobjc -save-temps
+"${PKG_TARG}-gcc" -o class-dump -ObjC *.m -Wall -include Foundation/Foundation.h -Wno-trigraphs -fpascal-strings -fasm-blocks -O0 -Wreturn-type -Wunused-variable -DHANDLE_OBJC2_ABI -framework CoreFoundation -lobjc -framework Foundation
 pkg: mkdir -p /usr/bin
 pkg: cp -a class-dump /usr/bin
