21cff62682a953750ac9bd7e65983060fa97b2c5
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-08-22 09:13:56 +0000

    Fixed BerkeleyDB mutex choice.

--- data/berkeleydb/make.sh
+++ data/berkeleydb/make.sh
@@ -1,5 +1,7 @@
 pkg:setup
-cd build_unix
-PKG_CONF=../dist/configure pkg:configure
+cd dist
+./s_config
+cd ../build_unix
+PKG_CONF=../dist/configure pkg:configure --with-mutex=Darwin/_spin_lock_try
 make
 pkg:install
