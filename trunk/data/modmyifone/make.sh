53275ff51dfc18accc2416f5d3c5141fd31f4365
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-09-14 17:16:48 +0000

    Moving ModMyiF0ne to a new repository.

--- trunk/data/modmyifone/make.sh
+++ trunk/data/modmyifone/make.sh
@@ -1,4 +1,4 @@
 pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/modmyifone.list /etc/apt/sources.list.d
+pkg: cp -a %/modmyi.com.list /etc/apt/sources.list.d
 pkg: mkdir -p /usr/share/keyrings
-pkg: cp -a %/186902.txt /usr/share/keyrings/modmyifone-keyring.gpg
+pkg: cp -a %/186902.txt /usr/share/keyrings/modmyi.com-keyring.gpg
