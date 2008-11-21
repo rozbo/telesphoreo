5e26912c346b83171d5824a1a0e3e3666e4b57eb
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-11-21 19:16:53 +0000

    While Cydia is not part of Telesphoreo, the bootstrapping tool is, and iPhoneOS 2.2 surprised me by being a day early.

---
+++ data/cydia/make.sh
@@ -0,0 +1,19 @@
+pkg:setup
+cd cydia
+make
+
+pkg: mkdir -p /usr/libexec /var/lib/cydia
+pkg: cp -a Library /usr/libexec/cydia
+pkg: cp -a exec /usr/libexec/cydia
+
+pkg: mkdir -p /System/Library
+pkg: cp -a LaunchDaemons /System/Library
+
+pkg: mkdir /Applications
+pkg: cp -a Cydia.app /Applications
+pkg: cp -a Cydia /Applications/Cydia.app/Cydia_
+ldid -S"${PKG_DATA}/cydia.xml" "${PKG_DEST}/Applications/Cydia.app/Cydia_"
+pkg: chmod +s /Applications/Cydia.app/Cydia_
+
+pkg: mkdir -p /System/Library/PreferenceBundles
+pkg: cp -a CydiaSettings.bundle /System/Library/PreferenceBundles
