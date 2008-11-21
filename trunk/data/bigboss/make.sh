20d1f2b8b20a4ec125a231b6f54dcd1eaab756ee
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-11-21 18:53:13 +0000

    I like sources, do you like sources?

--- trunk/data/bigboss/make.sh
+++ trunk/data/bigboss/make.sh
@@ -2,3 +2,5 @@ pkg: mkdir -p /etc/apt/sources.list.d
 pkg: cp -a %/bigboss.list /etc/apt/sources.list.d
 pkg: mkdir -p /usr/share/keyrings
 pkg: cp -a %/bigboss.asc /usr/share/keyrings/bigboss-keyring.gpg
+pkg: mkdir -p /usr/share/bigboss
+pkg: cp -a %/icons /usr/share/bigboss
