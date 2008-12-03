cb634469c601e8ea55bc9bde8d2d682a44ea8bdf
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2008-12-03 13:17:42 +0000

    Fixed OpenSSL for stunnel compilation.

--- trunk/data/stunnel/make.sh
+++ trunk/data/stunnel/make.sh
@@ -1,5 +1,5 @@
 pkg:setup
-pkg:configure ac_cv_file___dev_urandom_=yes ac_cv_file___dev_ptmx_=yes ac_cv_file___dev_ptc_=no --with-ssl="${PKG_ROOT}/usr"
+pkg:configure ac_cv_file___dev_urandom_=yes ac_cv_file___dev_ptmx_=yes ac_cv_file___dev_ptc_=no --with-ssl="$(PKG_DEST_ openssl)/usr"
 touch stunnel.pem
 make openssl="$(which openssl)"
 make install prefix="${PKG_DEST}/usr"
