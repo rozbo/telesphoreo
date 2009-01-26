be052bce2196c01632a51e8bdfcc5e76c1f6943e
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-01-26 13:05:25 +0000

    Forgot to commit some older repositories.

---
+++ trunk/data/ipuhelin.com/make.sh
@@ -0,0 +1,4 @@
+pkg: mkdir -p /etc/apt/sources.list.d
+pkg: cp -a %/ipuhelin.com.list /etc/apt/sources.list.d
+pkg: mkdir -p /Applications/Cydia.app/Sources
+pkg: cp -a %/ipuhelin.com.png /Applications/Cydia.app/Sources
