58ab2dbf90ff3dbb15ad14ed2fcb7b3befb81d6d
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-01-26 13:05:41 +0000

    Ugpraded GPG key for iFoneGuide.

--- data/ifoneguide.nl/make.sh
+++ data/ifoneguide.nl/make.sh
@@ -1,4 +1,4 @@
 pkg: mkdir -p /etc/apt/sources.list.d
 pkg: cp -a %/ifoneguide.nl.list /etc/apt/sources.list.d
 pkg: mkdir -p /usr/share/keyrings
-pkg: cp -a %/iFoneguide.dat /usr/share/keyrings/ifoneguide.nl-keyring.gpg
+pkg: cp -a %/iFoneguide.gpg /usr/share/keyrings/ifoneguide.nl-keyring.gpg
