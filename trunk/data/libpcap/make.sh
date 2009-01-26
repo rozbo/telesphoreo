436e3214ee2e9a190f02330019300cf14bc965f2
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-01-26 13:04:13 +0000

    Upgraded libpcap.

--- trunk/data/libpcap/make.sh
+++ trunk/data/libpcap/make.sh
@@ -2,4 +2,7 @@ pkg:setup
 pkg:configure --with-pcap=bpf
 make
 mkdir -p "${PKG_DEST}/usr/lib"
-make install-shared DESTDIR="${PKG_DEST}"
+pkg: mkdir -p /usr/bin
+make install install-shared DESTDIR="${PKG_DEST}"
+pkg: rm -f /usr/lib/libpcap.a
+pkg: ln -s libpcap.A.dylib /usr/lib/libpcap.0.dylib
