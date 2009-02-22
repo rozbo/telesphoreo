4f84d350cf62ae141d0a3aa2f34f8af983d6aa79
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-02-22 12:11:12 +0000

    Removed 10.4/10.5 libgcc split, that is an obsolete concept. (toelooper)

--- data/libgcc/make.sh
+++ data/libgcc/make.sh
@@ -1,6 +1,6 @@
 pkg: mkdir -p /usr/lib
 prefix=$(dirname "$(which "${PKG_TARG}-gcc")")/..
-for ver in 1 10.4 10.5; do
+for ver in 1; do
     libgcc=lib/libgcc_s.${ver}.dylib
     cp -a "${prefix}/${PKG_TARG}/${libgcc}" "${PKG_DEST}/usr/lib"
     #pkg: "${PKG_TARG}-strip" -x -no_uuid "/usr/${libgcc}"
