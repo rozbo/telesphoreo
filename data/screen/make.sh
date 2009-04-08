6975d32c2526d1318526c157dc1b58d7b1b69b9e
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-08 21:39:43 +0000

    Upgraded ncurses (and with it screen and top); dropped Essential: (*phew*) and moved to pututxline().

--- data/screen/make.sh
+++ data/screen/make.sh
@@ -1,6 +1,6 @@
 pkg:setup
 autoconf
-pkg:configure --disable-pam --with-sys-screenrc=/etc/screenrc
+pkg:configure --disable-pam --with-sys-screenrc=/etc/screenrc ac_cv_header_dwarf_h=yes
 make
 pkg:install
 pkg: mkdir -p /etc
