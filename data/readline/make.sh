deb5e6dc53e1cbaa6eeb2714820af4c5d704d6de
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-09 03:35:06 +0000

    Upgraded to readline 6.0, which I believe to be ABI compatible with 5.2 (and when I'm proved wrong I will /make/ it be, as I believe that is /correct/).

--- data/readline/make.sh
+++ data/readline/make.sh
@@ -2,3 +2,7 @@ pkg:setup
 pkg:configure ac_cv_func_strcoll_works=yes bash_cv_func_sigsetjmp=present bash_cv_func_ctype_nonascii=no bash_cv_must_reinstall_sighandlers=no bash_cv_func_strcoll_broken=yes
 make
 pkg:install
+pkg: ln -s libreadline.6.dylib /usr/lib/libreadline.5.dylib
+pkg: ln -s libreadline.6.0.dylib /usr/lib/libreadline.5.2.dylib
+pkg: ln -s libhistory.6.dylib /usr/lib/libhistory.5.dylib
+pkg: ln -s libhistory.6.0.dylib /usr/lib/libhistory.5.2.dylib
