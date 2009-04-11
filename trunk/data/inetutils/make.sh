dbc342a0ee1c3500c3a38d209708824609665aa3
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-11 00:51:56 +0000

    Finalized inetutils upgrade to 1.6.

--- trunk/data/inetutils/make.sh
+++ trunk/data/inetutils/make.sh
@@ -1,5 +1,6 @@
 pkg:setup
 autoconf
-pkg:configure ac_cv_type_sa_family_t=yes ac_cv_type_socklen_t=yes ac_cv_member_struct_utmp_ut_user=yes ac_cv_func_obstack=no --disable-ifconfig --disable-whois --disable-whois --disable-whois --disable-whois
-make
+# XXX: I miss talkd
+pkg:configure ac_cv_type_sa_family_t=yes ac_cv_type_socklen_t=yes ac_cv_member_struct_utmp_ut_user=yes ac_cv_func_obstack=no --disable-ifconfig --disable-ping6 --disable-syslogd --disable-talkd --disable-traceroute --disable-whois ac_cv_header_utmp_h=no
+make CFLAGS='-O2 -mthumb'
 pkg:install
