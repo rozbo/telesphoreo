fb6e2b055f5310fc7e35300a3300179a05f39c11
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-11 16:26:55 +0000

    Upgraded core archival utilities and noticed grep was ancient.

--- data/tar/make.sh
+++ data/tar/make.sh
@@ -1,6 +1,6 @@
 pkg:setup
-pkg:configure
-make
+pkg:configure --disable-dependency-tracking --disable-largefile
+make AR="${PKG_TARG}-ar" CFLAGS='-O2 -mthumb'
 pkg:install
 pkg:bin tar
 pkg: mkdir -p /usr/bin
