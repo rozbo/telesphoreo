4f19558073fadc8e4b3b0ad5d7be9f5d773ba051
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-11 02:54:43 +0000

    Some minor modifications to the linking of coreutils.

--- trunk/data/coreutils/make.sh
+++ trunk/data/coreutils/make.sh
@@ -3,10 +3,11 @@ pkg:configure --enable-install-program=su
 make AR="${PKG_TARG}-ar" CFLAGS='-O2 -mthumb'
 pkg:install
 pkg: rm -f /usr/bin/sync
-pkg:bin cat chgrp chmod chown cp date dd df dir echo false kill ln ls mkdir mknod mv pwd readlink rm rmdir sleep stty su touch true uname vdir
+pkg:bin cat chgrp chmod chown cp date dd df dir echo false kill ln ls mkdir mknod mktemp mv pwd readlink rm rmdir sleep stty su touch true uname vdir
 pkg: mkdir -p /usr/sbin
 pkg: mv /usr/bin/chroot /usr/sbin
 pkg: mkdir -p /etc/profile.d
 pkg: cp -a %/coreutils.sh /etc/profile.d
-pkg: cp -a /bin/chown /usr/bin/chown
-pkg: cp -a /bin/chown /usr/sbin/chown
+ln -s /bin/chown "${PKG_DEST}"/usr/bin
+ln -s /bin/chown "${PKG_DEST}"/usr/sbin
+ln -s /bin/mktemp "${PKG_DEST}"/usr/bin
