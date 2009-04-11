05814f2a84f2b2336ebcf2336ebbc4a78e88a4b4
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-11 00:29:42 +0000

    Upgraded to coreutils 7.2.

--- trunk/data/coreutils/make.sh
+++ trunk/data/coreutils/make.sh
@@ -1,9 +1,9 @@
 pkg:setup
-pkg:configure
-make
+pkg:configure --enable-install-program=su
+make AR="${PKG_TARG}-ar" CFLAGS='-O2 -mthumb'
 pkg:install
 pkg: rm -f /usr/bin/sync
-pkg:bin cat chgrp chmod chown cp date dd df dir echo false hostname kill ln ls mkdir mknod mv pwd readlink rm rmdir vdir sleep stty su touch true uname
+pkg:bin cat chgrp chmod chown cp date dd df dir echo false kill ln ls mkdir mknod mv pwd readlink rm rmdir sleep stty su touch true uname vdir
 pkg: mkdir -p /usr/sbin
 pkg: mv /usr/bin/chroot /usr/sbin
 pkg: mkdir -p /etc/profile.d
