290d19ed3d836f2b1ebdd1f47c38d3071701c7ed
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-11 00:29:42 +0000

    Upgraded to coreutils 7.2.

--- data/shell-cmds/make.sh
+++ data/shell-cmds/make.sh
@@ -1,7 +1,6 @@
 pkg:setup
 mkdir -p "${PKG_DEST}/usr/bin"
-for bin in killall mktemp renice script time which; do
-    ${PKG_TARG}-gcc -o "${bin}/${bin}" "${bin}"/*.c -D'__FBSDID(x)=' -save-temps
+for bin in killall renice script time which; do
+    ${PKG_TARG}-gcc -O2 -mthumb -o "${bin}/${bin}" "${bin}"/*.c -D'__FBSDID(x)=' -save-temps
     cp -a "${bin}/${bin}" "${PKG_DEST}/usr/bin"
 done
-pkg:bin mktemp
