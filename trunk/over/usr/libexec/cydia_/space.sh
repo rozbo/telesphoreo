ac005228b6e989ad46a3f52f78f864b2d4b84655
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-11 05:06:03 +0000

    Removed move.sh from space.sh (forever ago).

--- trunk/over/usr/libexec/cydia_/space.sh
+++ trunk/over/usr/libexec/cydia_/space.sh
@@ -1,14 +1,4 @@
 #!/usr/libexec/cydia_/bash
 export PATH=/usr/libexec/cydia_
-
-for dir in \
-    /Applications \
-    /Library/Ringtones \
-    /Library/Wallpaper \
-    /System/Library/Fonts \
-    /usr/share
-do
-    . /usr/libexec/cydia_/move.sh "${dir}"
-done
-
+. /usr/libexec/cydia/space.sh
 [[ $(df_ /) -gt 35651584 && $(df_ /var) -gt 35651584 ]]
