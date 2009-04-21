2b5689e0b05bcd38d363b411b6f736a3accfaba8
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-04-21 09:23:41 +0000

    Cornerstone of the new effort.

--- trunk/data/dpkg/make.sh
+++ trunk/data/dpkg/make.sh
@@ -1,6 +1,12 @@
 pkg:setup
 autoconf
-cp -a getopt/obstack.[ch] lib
+cp -a libcompat/obstack.[ch] lib
 pkg:configure --with-admindir=/var/lib/dpkg --without-start-stop-daemon --disable-nls --sysconfdir=/etc --disable-linker-optimisations dpkg_cv_va_copy=yes
-make
+pkg:make
 pkg:install
+pkg: rm -rf /usr/share/perl5
+pkg: grep -Erl '#! ?/usr/bin/perl' / | while read -r line; do
+    rm -f "${line}"
+done
+pkg: rm -rf /{usr,var}/lib/dpkg/methods
+pkg: rm -rf /usr/lib/dpkg/parsechangelog
