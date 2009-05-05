3b17470b9e6f268a59eb0a087ce662cd2a363489
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-05-05 05:42:01 +0000

    Upgrading default repository packages to normal structure.

--- data/bigboss/make.sh
+++ data/bigboss/make.sh
@@ -2,5 +2,7 @@ pkg: mkdir -p /etc/apt/sources.list.d
 pkg: cp -a %/bigboss.list /etc/apt/sources.list.d
 pkg: mkdir -p /usr/share/keyrings
 pkg: cp -a %/bigboss.asc /usr/share/keyrings/bigboss-keyring.gpg
+pkg: mkdir -p /Applications/Cydia.app/Sources
+pkg: cp -a %/apt.bigboss.us.com.png /Applications/Cydia.app/Sources/apt.bigboss.us.com.png
 pkg: mkdir -p /usr/share/bigboss
 pkg: cp -a %/icons /usr/share/bigboss
