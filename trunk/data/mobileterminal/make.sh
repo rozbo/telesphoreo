dfd041706ddd3198125469d4d5b545bddcb99c69
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-05-11 22:37:25 +0000

    Upgraded MobileTerminal for 3.x.

--- trunk/data/mobileterminal/make.sh
+++ trunk/data/mobileterminal/make.sh
@@ -1,6 +1,6 @@
 pkg:setup
 echo "#define SVN_VERSION @\"${PKG_VERS}\"" >svnversion.h
 make clean
-make -f Makefile.build CC="${PKG_TARG}-gcc"
+make CC="${PKG_TARG}-gcc"
 pkg: mkdir -p /Applications/Terminal.app
 pkg: cp -a Info.plist Resources/* Terminal /Applications/Terminal.app
