a79fdb32ec150f69987a23d650edf9fa79db1f2e
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-05-31 05:59:47 +0000

    Apparently I never committed some of the recent APT reorganizations.

---
+++ trunk/data/apt7-ssl/make.sh
@@ -0,0 +1,2 @@
+pkg: mkdir -p /usr/lib/apt/methods
+cp -a "$(PKG_DEST_ _apt7)"/usr/lib/apt/methods/https "${PKG_DEST}"/usr/lib/apt/methods
---
+++ trunk/data/apt7/make.sh
@@ -0,0 +1,3 @@
+pkg: mkdir -p /usr
+cp -a "$(PKG_DEST_ _apt7)"/usr/bin "${PKG_DEST}"/usr
+cp -a "$(PKG_DEST_ _apt7)"/usr/include "${PKG_DEST}"/usr
