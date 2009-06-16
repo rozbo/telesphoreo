8dfd47030649a9006aa6a8a289ee7fb01d18e2fb
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-06-16 22:50:56 +0000

    Checkpointing ffmpeg (no longer supported).

--- trunk/data/ffmpeg_/make.sh
+++ trunk/data/ffmpeg_/make.sh
@@ -4,7 +4,7 @@ pkg:setup
     --disable-vhook --enable-avfilter --enable-avfilter-lavf \
     --enable-cross-compile --cross-prefix=${PKG_TARG}- --target-os=darwin \
     --arch=arm --disable-iwmmxt --enable-armv5te --enable-armv6 \
-    --enable-static --disable-shared --disable-debug --disable-stripping \
+    --disable-static --enable-shared --disable-debug --disable-stripping \
     --enable-libfaac \
     --enable-libfaad \
     --enable-libmp3lame \
