486b096a20d1b36610da60bfd936bc6348bac599
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-07-16 13:30:29 +0000

    I apparently wanted this stuff.

--- trunk/data/samba/make.sh
+++ trunk/data/samba/make.sh
@@ -1,7 +1,24 @@
 pkg:setup
 cd source
 ./autogen.sh
-pkg:configure samba_cv_CC_NEGATIVE_ENUM_VALUES=yes
+pkg:configure samba_cv_CC_NEGATIVE_ENUM_VALUES=yes --without-cluster-support --disable-swat --without-ldap --with-configdir=/etc/samba --with-privatedir=/etc/samba --with-fhs
 make
-pkg:install
-pkg: rm -rf /usr/swat
+pkg:install BIN_PROGS4=
+pkg: mkdir /usr/samba
+
+function smb:move() {
+    usr=$1
+    rex=$2
+    pkg: mkdir /usr/samba/"${usr}"
+    for bin in "${PKG_DEST}"/usr/"${usr}"/*; do
+        bin=${bin##*/}
+        if [[ ${bin} =~ ${rex} ]]; then
+            pkg: mv /usr/"${usr}"/"${bin}" /usr/samba/"${usr}"
+            ln -s /usr/samba/"${usr}"/"${bin}" "${PKG_DEST}"/usr/"${usr}"/"${bin}"
+        fi
+    done
+}
+
+smb:move bin '.*'
+smb:move lib '.*\.dylib\.[0-9]'
+smb:move sbin '.*'
