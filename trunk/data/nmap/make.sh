4c9c2c1ec7a7bce865c5e53361abae71986c3a91
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-07-30 21:44:17 +0000

    Upgraded to nmap 5.00.

--- trunk/data/nmap/make.sh
+++ trunk/data/nmap/make.sh
@@ -2,7 +2,9 @@ pkg:setup
 autoconf
 cd libdnet-stripped
 autoconf
+cd ../ncat
+autoconf
 cd ..
 pkg:configure --without-liblua --without-python --without-zenmap --enable-static=yes --with-liblua=no
-make
+make AR="${PKG_TARG}-ar"
 pkg:install
