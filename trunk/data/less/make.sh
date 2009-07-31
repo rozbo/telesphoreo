6422af846b4050e6f235256df9a63eaac0b10e37
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-07-31 03:09:28 +0000

    Upgraded to less 418.

--- trunk/data/less/make.sh
+++ trunk/data/less/make.sh
@@ -1,6 +1,8 @@
 pkg:setup
 pkg:configure
-make
+pkg:make
 pkg:install
-pkg: mkdir -p /etc/profile.d
+pkg: mkdir -p /etc/profile.d /bin
 pkg: cp -a %/less.sh /etc/profile.d
+pkg: ln -s less /usr/bin/more
+ln -s /usr/bin/less "${PKG_DEST}"/bin/more
