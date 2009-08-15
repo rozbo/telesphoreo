96c83c50d70ba45c5e5dbde7696049e1233f42ff
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-08-15 08:54:20 +0000

    Various things I forgot to commit.

--- data/ispazio.net/make.sh
+++ data/ispazio.net/make.sh
@@ -1,2 +1,4 @@
 pkg: mkdir -p /etc/apt/sources.list.d
 pkg: cp -a %/ispazio.net.list /etc/apt/sources.list.d
+pkg: mkdir -p /Applications/Cydia.app/Sources
+pkg: cp -a %/ispaziorepo.com.png /Applications/Cydia.app/Sources/www.ispaziorepo.com.png
