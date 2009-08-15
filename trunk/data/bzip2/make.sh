e2f89be12384e2547593b8754e2c2e1749d17c26
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-08-15 08:53:01 +0000

    Various things I forgot to commit.

--- trunk/data/bzip2/make.sh
+++ trunk/data/bzip2/make.sh
@@ -1,5 +1,5 @@
 pkg:setup
-make bzip2 bzip2recover CC=${PKG_TARG}-gcc
+make bzip2 bzip2recover CC=${PKG_TARG}-gcc CFLAGS='-Wall -Winline -O2'
 pkg: mkdir -p /bin
 pkg: cp -a bzip2 bzip2recover /bin
 pkg: ln -s bzip2 /bin/bunzip2
