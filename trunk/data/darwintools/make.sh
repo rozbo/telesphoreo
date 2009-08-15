c2944519b89213ad6ac9a889b4a54c9c107b33fd
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-08-15 08:59:06 +0000

    Various things I forgot to commit.

--- trunk/data/darwintools/make.sh
+++ trunk/data/darwintools/make.sh
@@ -1,7 +1,8 @@
 pkg:setup
-pkg: mkdir -p /usr/bin
 
-for tool in startupfiletool sw_vers; do
-    "${PKG_TARG}-gcc" -o "${tool}" "${tool}.c" -framework CoreFoundation
-    pkg: cp -a "${tool}" /usr/bin
-done
+"${PKG_TARG}-gcc" -O2 -mthumb -o startupfiletool startupfiletool.c
+"${PKG_TARG}-gcc" -O2 -mthumb -o sw_vers sw_vers.c -framework CoreFoundation
+
+pkg: mkdir -p /usr/{,s}bin
+pkg: cp -a sw_vers /usr/bin
+pkg: cp -a startupfiletool /usr/sbin
