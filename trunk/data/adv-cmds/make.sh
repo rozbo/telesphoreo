6b1f9c4a22f9a82f9a58bd3f344c6c0ab319f006
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-08-15 08:51:37 +0000

    Various updates I forgot to commit.

--- trunk/data/adv-cmds/make.sh
+++ trunk/data/adv-cmds/make.sh
@@ -1,10 +1,12 @@
 pkg:setup
+"${PKG_TARG}-gcc" -O2 -mthumb -o tabs tabs.tproj/*.c -lncurses
+"${PKG_TARG}-g++" -O2 -mthumb -o locale/locale locale/*.cc
 for tproj in finger fingerd last lsvfs md ps; do
-    "${PKG_TARG}-gcc" -o "${tproj}" "${tproj}.tproj"/*.c -D'__FBSDID(x)='
+    echo "${tproj}"
+    "${PKG_TARG}-gcc" -O2 -mthumb -o "${tproj}" "${tproj}.tproj"/*.c -D'__FBSDID(x)='
 done
-"${PKG_TARG}-gcc" -o tabs tabs.tproj/*.c -lncurses
 pkg: mkdir -p /bin /usr/bin /usr/libexec
 pkg: cp -a ps /bin
-pkg: cp -a finger last lsvfs md tabs /usr/bin
+pkg: cp -a finger last locale/locale lsvfs md tabs /usr/bin
 pkg: cp -a fingerd /usr/libexec
 ldid -S"${PKG_DATA}/ps.xml" "${PKG_DEST}/bin/ps"
