952ca694c315a1e74e269e6574a073e84b307683
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-09-08 06:07:43 +0000

    Removing some obsolete repositories.

--- trunk/data/a-steroids.com/make.sh
+++
@@ -1,4 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/a-steroids.com.list /etc/apt/sources.list.d
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/logo.png /Applications/Cydia.app/Sources/a-steroids.com.png
--- trunk/data/richcreations.com/make.sh
+++
@@ -1,2 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/richcreations.com.list /etc/apt/sources.list.d
