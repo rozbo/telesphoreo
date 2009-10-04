935c40eb6e48f260684817acb7f5197b8698e691
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2009-10-04 05:21:31 +0000

    Removing ipuhelin.

--- trunk/data/ipuhelin.com/make.sh
+++
@@ -1,4 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/ipuhelin.com.list /etc/apt/sources.list.d
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/ipuhelin.com.png /Applications/Cydia.app/Sources
