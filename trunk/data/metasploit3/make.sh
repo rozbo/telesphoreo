023b76aff9aaafcf45c2ac0b4baff2a63a5036e5
extremis <extremis@514c082c-b64e-11dc-b46d-3d985efe055d>
2010-01-19 17:38:59 +0000

    Downgraded to stable and leaving in .svn files for updates

--- trunk/data/metasploit3/make.sh
+++ trunk/data/metasploit3/make.sh
@@ -1,7 +1,6 @@
 pkg:extract
 pkg: mkdir -p /usr/share /usr/bin
 pkg: cp -a * /usr/share/msf3
-pkg: find / -name '.svn' -prune -exec rm -rfv {} \;
 pkg: "${PKG_TARG}-gcc" -o /usr/share/msf3/data/templates/template_armle_darwin.bin /usr/share/msf3/data/templates/template.c
 ln -s /usr/share/msf3/msfcli "${PKG_DEST}"/usr/bin/msfcli
 ln -s /usr/share/msf3/msfconsole "${PKG_DEST}"/usr/bin/msfconsole
