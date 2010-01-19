09166580aac12aa9c8cb108e9f1a5455afc89c79
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2010-01-19 13:26:13 +0000

    Removed an obsolete repository.

--- data/rupertgee/make.sh
+++
@@ -1,6 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/rupertgee.list /etc/apt/sources.list.d
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/redwolfberry.com.png /Applications/Cydia.app/Sources
-pkg: mkdir -p /usr/share/keyrings
-pkg: cp -a %/rupertgee-keyring.gpg /usr/share/keyrings/rupertgee-keyring.gpg
