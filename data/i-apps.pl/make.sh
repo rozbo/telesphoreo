92695e63ad5ee0588d16ac679912894d86b67c5f
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2010-03-29 05:30:58 +0000

    Removing obsolete repositories.

--- data/i-apps.pl/make.sh
+++
@@ -1,2 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/i-apps.pl.list /etc/apt/sources.list.d
--- data/weho.ru/make.sh
+++
@@ -1,2 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/weho.ru.list /etc/apt/sources.list.d
