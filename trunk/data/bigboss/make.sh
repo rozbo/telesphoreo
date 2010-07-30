c7e1613a8ae15236afe8255a9a4be630b030933d
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2010-07-30 10:22:43 +0000

    We should commit BigBoss.

--- trunk/data/bigboss/make.sh
+++ trunk/data/bigboss/make.sh
@@ -4,5 +4,6 @@ pkg: mkdir -p /usr/share/keyrings
 pkg: cp -a %/bigboss.asc /usr/share/keyrings/bigboss-keyring.gpg
 pkg: mkdir -p /Applications/Cydia.app/Sources
 pkg: cp -a %/apt.bigboss.us.com.png /Applications/Cydia.app/Sources/apt.bigboss.us.com.png
+pkg: cp -a %/apt.bigboss.us.com.png /Applications/Cydia.app/Sources/apt.thebigboss.org.png
 pkg: mkdir -p /usr/share/bigboss
 pkg: cp -a %/icons /usr/share/bigboss
