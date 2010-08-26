2b486c79463e925ecf3f1c670d39a0304f71b6f9
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2010-08-26 18:55:17 +0000

    Remove iSpazio.

--- trunk/data/ispazio.net/make.sh
+++
@@ -1,4 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/ispazio.net.list /etc/apt/sources.list.d
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/ispaziorepo.com.png /Applications/Cydia.app/Sources/www.ispaziorepo.com.png
