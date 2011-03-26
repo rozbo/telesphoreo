e6a4c89e8ce23396f2162216e83e65975a6a51de
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2011-03-26 14:08:43 +0000

    Removing old source packages.

--- data/bigboss/make.sh
+++
@@ -1,3 +0,0 @@
-pkg: mkdir -p /usr/share/bigboss
-pkg: cp -a %/icons /usr/share/bigboss
-pkg: rm -rf /usr/share/bigboss/icons/.svn
--- data/yellowsn0w.com/make.sh
+++
@@ -1,4 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/yellowsn0w.com.list /etc/apt/sources.list.d
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/yellowsn0w.com.png /Applications/Cydia.app/Sources/apt9.yellowsn0w.com.png
