50b27f7ddfd2c7f622351f3c7ad9722238e32e16
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2011-03-26 14:07:05 +0000

    Checkpoint old source packages before removing them.

--- data/bigboss/make.sh
+++ data/bigboss/make.sh
@@ -1,9 +1,3 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/bigboss.list /etc/apt/sources.list.d
-pkg: mkdir -p /usr/share/keyrings
-pkg: cp -a %/bigboss.asc /usr/share/keyrings/bigboss-keyring.gpg
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/apt.bigboss.us.com.png /Applications/Cydia.app/Sources/apt.bigboss.us.com.png
-pkg: cp -a %/apt.bigboss.us.com.png /Applications/Cydia.app/Sources/apt.thebigboss.org.png
 pkg: mkdir -p /usr/share/bigboss
 pkg: cp -a %/icons /usr/share/bigboss
+pkg: rm -rf /usr/share/bigboss/icons/.svn
---
+++ data/yellowsn0w.com/make.sh
@@ -0,0 +1,4 @@
+pkg: mkdir -p /etc/apt/sources.list.d
+pkg: cp -a %/yellowsn0w.com.list /etc/apt/sources.list.d
+pkg: mkdir -p /Applications/Cydia.app/Sources
+pkg: cp -a %/yellowsn0w.com.png /Applications/Cydia.app/Sources/apt9.yellowsn0w.com.png
