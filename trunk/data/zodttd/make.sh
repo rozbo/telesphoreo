fda2620bef9269dffcceae5db887b7774f971423
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2011-03-26 14:08:43 +0000

    Removing old source packages.

--- trunk/data/zodttd/make.sh
+++
@@ -1,6 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/zodttd.list /etc/apt/sources.list.d
-pkg: mkdir -p /usr/share/keyrings
-pkg: cp -a %/failkey.asc /usr/share/keyrings/zodttd-keyring.gpg
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/www.zodttd.com.png /Applications/Cydia.app/Sources/cydia.zodttd.com.png
