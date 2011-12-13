96918bd1380eda96bd4714f2ad7547c631acb307
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2011-12-13 17:11:13 +0000

    Removing obsolete repositories.

--- data/apple-news.fr/make.sh
+++
@@ -1,4 +0,0 @@
-pkg: mkdir -p /etc/apt/sources.list.d
-pkg: cp -a %/apple-news.fr.list /etc/apt/sources.list.d
-pkg: mkdir -p /Applications/Cydia.app/Sources
-pkg: cp -a %/apple-news.fr.png /Applications/Cydia.app/Sources/apple-news.fr.png
