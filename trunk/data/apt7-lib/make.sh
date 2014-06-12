e656970ae4c4c8c3b9aa36bacade2efa564378f6
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2014-06-12 15:15:52 +0000

    Replace apt7-ssl with apt7-lib.

--- trunk/data/apt7-lib/make.sh
+++ trunk/data/apt7-lib/make.sh
@@ -4,3 +4,4 @@ cp -a "$(PKG_DEST_ _apt7)"/var "${PKG_DEST}"
 pkg: mkdir -p /usr
 cp -a "$(PKG_DEST_ _apt7)"/usr/lib "${PKG_DEST}"/usr
 pkg: rm -f /usr/lib/apt/methods/https
+pkg: ln -s http /usr/lib/apt/methods/https
