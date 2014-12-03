91cb2f0e9ad42361be6dc137383a055c6819d73a
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2014-12-03 11:51:27 +0000

    Upgrade to the latest version of git.

--- trunk/data/git/make.sh
+++ trunk/data/git/make.sh
@@ -1,5 +1,6 @@
 pkg:setup
+export PATH=/apl/tel/path:$PATH
 pkg:configure ac_cv_c_c99_format=yes ac_cv_fread_reads_directories=no ac_cv_snprintf_returns_bogus=yes --without-tcltk
 make
 pkg:install
-pkg: sed -i -e 's/^=head2 .*:/=head2/' /usr/lib/perl/5.8.8/perllocal.pod
+pkg: rm -f /usr/lib/perl/5.10.1/perllocal.pod
