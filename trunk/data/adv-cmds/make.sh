6107c3b8a006f2428f1c247c59547b9743e02eda
saurik <saurik@514c082c-b64e-11dc-b46d-3d985efe055d>
2015-10-13 09:40:31 +0000

    Move ps to /usr/bin to protect iOS 9.

--- trunk/data/adv-cmds/make.sh
+++ trunk/data/adv-cmds/make.sh
@@ -6,7 +6,7 @@ for tproj in finger fingerd last lsvfs md ps; do
     "${PKG_TARG}-gcc" -O2 -mthumb -o "${tproj}" "${tproj}.tproj"/*.c -D'__FBSDID(x)='
 done
 pkg: mkdir -p /bin /usr/bin /usr/libexec
-pkg: cp -a ps /bin
+pkg: cp -a ps /usr/bin
 pkg: cp -a finger last locale/locale lsvfs md tabs /usr/bin
 pkg: cp -a fingerd /usr/libexec
-ldid -S"${PKG_DATA}/ps.xml" "${PKG_DEST}/bin/ps"
+ldid -S"${PKG_DATA}/ps.xml" "${PKG_DEST}/usr/bin/ps"
